77c1e7cb037af6cad6c45a1decf6187d8ae9c9c4
luizasuyoko <luizasuemiyokomizo@outlook.com>
2025-09-09 13:38:06 -0300

    09.09.2025

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsService } from './products.service';
+import { ProductEntity } from './entities/product.entity';
 
 @Module({
-    controllers: [ProductsController]
+    imports: [TypeOrmModule.forFeature([ProductEntity])], 
+    controllers: [ProductsController],
+    providers: [ProductsService],
 })
 export class ProductsModule {}
